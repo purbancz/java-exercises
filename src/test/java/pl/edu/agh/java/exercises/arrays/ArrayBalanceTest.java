66bdfb26b0134d9d9f730278ae2c156cb45d2b34
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2019-02-14 00:02:01 +0100

    ExArr 1 - Comments added

--- src/test/java/pl/edu/agh/java/exercises/arrays/ArrayBalanceTest.java
+++ src/test/java/pl/edu/agh/java/exercises/arrays/ArrayBalanceTest.java
@@ -18,28 +18,33 @@ public class ArrayBalanceTest {
     @Parameters
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-                {new int[]{}, true},
-                {new int[]{1}, false},
-                {new int[]{1, 1}, true},
-                {new int[]{1, 2}, false},
-                {new int[]{1, 1, 2}, true},
-                {new int[]{1, 2, 2}, false},
-                {new int[]{1, 2, 3}, true},
-                {new int[]{33, 33, 1, 2, 1}, true},
-                {new int[]{2, 1, 1, 2, 1}, false},
-                {new int[]{3, 4, 5, 6}, true},
-                
-                {new int[]{1, 1, 1, 2, 1}, true},	
-        		{new int[]{2, 1, 1, 2, 1}, false},	
-				{new int[]{10, 10}, true},
-				{new int[]{10, 0, 1, -1, 10}, true},	
-				{new int[]{1, 1, 1, 1, 4}, true},	
-				{new int[]{2, 1, 1, 1, 4}, false},	
-				{new int[]{2, 3, 4, 1, 2}, true},	
-				{new int[]{1, 2, 3, 1, 0, 2, 3}, true},	
-				{new int[]{1, 2, 3, 1, 0, 1, 3}, false},	
-				{new int[]{1}, false},	
-				{new int[]{1, 1, 1, 2, 1}, true},
+        	
+// poprawione
+//                {new int[]{}, true},
+//                {new int[]{1}, false},
+//                {new int[]{1, 1}, true},
+//                {new int[]{1, 2}, false},
+//                {new int[]{1, 1, 2}, true},
+//                {new int[]{1, 2, 2}, false},
+//                {new int[]{1, 2, 3}, true},
+//                {new int[]{33, 33, 1, 2, 1}, true},
+//                {new int[]{2, 1, 1, 2, 1}, false},
+//                {new int[]{3, 4, 5, 6}, true},
+//                
+// ze strony
+//              {new int[]{1, 1, 1, 2, 1}, true},	
+//        		{new int[]{2, 1, 1, 2, 1}, false},	
+//				{new int[]{10, 10}, true},
+//				{new int[]{10, 0, 1, -1, 10}, true},	
+//				{new int[]{1, 1, 1, 1, 4}, true},	
+//				{new int[]{2, 1, 1, 1, 4}, false},	
+//				{new int[]{2, 3, 4, 1, 2}, true},	
+//				{new int[]{1, 2, 3, 1, 0, 2, 3}, true},	
+//				{new int[]{1, 2, 3, 1, 0, 1, 3}, false},	
+//				{new int[]{1}, false},	
+//				{new int[]{1, 1, 1, 2, 1}, true},
+        	
+// oryginalne
 				
 				{new int[]{}, true},
                 {new int[]{1}, true},
