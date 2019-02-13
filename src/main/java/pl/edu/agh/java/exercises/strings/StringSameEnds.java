63c44b2856319dcdefb360ef8bc1c7d5860be84c
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2019-02-13 23:38:43 +0100

    ExSt1

--- src/main/java/pl/edu/agh/java/exercises/strings/StringSameEnds.java
+++ src/main/java/pl/edu/agh/java/exercises/strings/StringSameEnds.java
@@ -1,16 +1,30 @@
 package pl.edu.agh.java.exercises.strings;
 
 /**
- * Given a string, return the longest substring that appears at both the beginning and end of the string without
- * overlapping. For example, sameEnds("abXab") is "ab".
+ * Given a string, return the longest substring that appears at both the
+ * beginning and end of the string without overlapping. For example,
+ * sameEnds("abXab") is "ab".
  * <p>
- * Dla danego ciągu znaków zwróć najdłuższy podciąg, który występuje na początku i na końcu, bez nachodzenia na siebie.
- * Przykłądowo, sameEnds("abXab") to "ab".
+ * Dla danego ciągu znaków zwróć najdłuższy podciąg, który występuje na początku
+ * i na końcu, bez nachodzenia na siebie. Przykłądowo, sameEnds("abXab") to
+ * "ab".
  *
  * @see http://codingbat.com/prob/p131516
  */
 public class StringSameEnds {
-    public String sameEnds(String string) {
-        throw new UnsupportedOperationException();
-    }
+	public String sameEnds(String string) {
+		String same = "";
+		int idx = 1;
+		for (int i = string.length() / 2; i < string.length(); i++) {
+			if (string.charAt(i) == string.charAt(0) && i > idx) {
+				idx = i;
+			}
+		}
+		for (int i = idx; i < string.length(); i++) {
+			if (string.charAt(0 + i - idx) == string.charAt(i)) {
+				same += string.charAt(0 + i);
+			}
+		}
+		return same;
+	}
 }
