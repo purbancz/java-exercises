af297c04821ff42012c2bef29bc930bb8cf98cec
Piotr Urbanczyk <p.m.urbanczyk@gmail.com>
2019-02-13 23:55:08 +0100

    All ex's

--- src/main/java/pl/edu/agh/java/exercises/strings/StringSumDigits.java
+++ src/main/java/pl/edu/agh/java/exercises/strings/StringSumDigits.java
@@ -1,21 +1,26 @@
 package pl.edu.agh.java.exercises.strings;
 
 /**
- * Given a string, return the sum of the digits 0-9 that appear in the string, ignoring all other characters. Return 0
- * if there are no digits in the string.
- * Note:
- * Character.isDigit(char) tests if a char is one of the chars '0', '1', .. '9'.
- * Integer.parseInt(string) converts a string to an int.
+ * Given a string, return the sum of the digits 0-9 that appear in the string,
+ * ignoring all other characters. Return 0 if there are no digits in the string.
+ * Note: Character.isDigit(char) tests if a char is one of the chars '0', '1',
+ * .. '9'. Integer.parseInt(string) converts a string to an int.
  * <p>
- * Dla danego ciągu znaków zwróć sumę cyfr 0-9, które w nim wystąpią. Zwróć 0 jeśli ciąg znaków nie zawiera żadnych cyfr.
- * Warto wiedzieć:
- * Character.isDigit(char) sprawdza czy dany znak jest jednym ze znaków '0', '1', .. '9'.
- * Integer.parseInt(string) konwertuje string to liczby całkowitej.
+ * Dla danego ciągu znaków zwróć sumę cyfr 0-9, które w nim wystąpią. Zwróć 0
+ * jeśli ciąg znaków nie zawiera żadnych cyfr. Warto wiedzieć:
+ * Character.isDigit(char) sprawdza czy dany znak jest jednym ze znaków '0',
+ * '1', .. '9'. Integer.parseInt(string) konwertuje string to liczby całkowitej.
  *
  * @see http://codingbat.com/prob/p197890
  */
 public class StringSumDigits {
-    public int sumDigits(String str) {
-        throw new UnsupportedOperationException();
-    }
+	public int sumDigits(String str) {
+		int sum = 0;
+		for (char ch : str.toCharArray()) {
+			if (Character.isDigit(ch)) {
+				sum += ch - '0';
+			}
+		}
+		return sum;
+	}
 }
